8c1a2bb5728c55771bec4509d272c2c39007ccc6
Saltanat <sultanak2910@gmail.com>
2021-03-18 14:23:33 +0600

    aboutus

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -3,7 +3,7 @@ import './Logo.scss';
 import { FaDumbbell } from 'react-icons/fa';
 function Logo() {
   return (
-    <h1 className="logo"> <FaDumbbell className="logo-icon" /> <span>SaltaFitnFood</span></h1>
+    <h1 className="logo"> <FaDumbbell className="logo-icon" /> <span>Muscle</span></h1>
   )
 }
 
--- src/container/AboutUs/AboutUs.js
+++ src/container/AboutUs/AboutUs.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import Bgimg from '../../assets/img/aboutusbg.jpg';
+import './AboutUs.scss';
 
 function AboutUs() {
   return (
-    <div>
-      <h1 className="about-title">About Us</h1>
+    <div className="about">
+      <div className="about-head">
+        <h1 className="about-title">О Нас</h1>
+        <img src={Bgimg} alt=" " className="about-img" />
+      </div>
+      <div className="about-text">
+        <p className="about-text">Фитнесс клуб "Muscle" - это место, где сочетание фитнесс зоны для тренировок и зоны отдыха стало обыденностью. Занимаясь у нас вы будете ощущать себя проработавшим и одновременно отдохнувшим. Мы постарались сделать все, чтобы вы чувствовали себя комфортно.</p>
+        <p className="about-text low">Добро пожаловать в фитнесс клуб "Muscle"</p>
+      </div>
+
     </div>
   )
 }
